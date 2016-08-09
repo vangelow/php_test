6959bfddabbaa8a180c5998ac28b3ab64b456054
vangelow <victorangelov1@gmail.com>
2016-08-09 15:09:18 +0300

    Added 2 changes

--- script.js
+++ script.js
@@ -7,90 +7,6 @@ const kinveyAppSecret = '04dfa8a0163243b499bcbd3012531b82';
 const kinveyAppServiceUrl = 'https://baas.kinvey.com/';
 
 
-
-function showView(viewId) {
-    $("main>section").hide();
-
-    $("#" + viewId).show();
-}
-function showHideNavigationLinks(){
-    let loggedIn = sessionStorage.authToken != null;
-    if (loggedIn){
-
-        $("#LinkLogin").hide();
-        $("#LinkRegister").hide();
-        $("#LinkListBooks").show();
-        $("#LinkCreateBook").show();
-        $("#LinkLogout").show();
-    }
-    else{
-        $("#LinkLogin").show();
-        $("#LinkRegister").show();
-        $("#LinkListBooks").hide();
-        $("#LinkCreateBook").hide();
-        $("#LinkLogout").hide();
-    }
-}
-function showHomeView(){
-showView('viewHome');
-}
-function showLoginView(){
-showView('viewLogin');
-}
-function login()
-{
-
-let authBase64 = btoa(kinveyAppID + ":" + kinveyAppSecret);
-
-    $.ajax({
-        method: "POST",
-        url: kinveyAppServiceUrl + "user/" +kinveyAppID + "/login",
-        headers : {"Authorization" : "Basic " + authBase64},
-        data:
-        {
-            username: $("#loginUser").val(),
-            password: $("#loginPass").val()
-        },
-        success: loginSuccess,
-        error: showAjaxError
-    });
-    function loginSuccess(data, status) {
-
-        sessionStorage.authToken = data._kmd.authtoken;
-        showHideNavigationLinks();
-        showHomeView();
-        showInfo("Login successful")
-
-    }}
-function showInfo(msgText) {
-    $("#infoBox").text(msgText).show().delay(3000).fadeOut();
-}
-function showAjaxError(data, status) {
-    let errorMsg ='';
-    if(typeof(data.readyState) != 'undefined' && data.readyState ==0)
-        errorMsg = "Network error.";
-    else if(data.responseJSON && data.responseJSON.description)
-errorMsg = data.responseJSON.description;
-    else
-errorMsg = "Error: " + JSON.stringify(data);
-    $('#infoBox').text(errorMsg).show();
-}
-function showRegisterView(){
-showView('viewRegister');
-
-}
-
-function register()
-{
-
-    let authBase64 = btoa(kinveyAppID + ":" + kinveyAppSecret);
-
-    $.ajax({
-        method: "POST",
-        url: kinveyAppServiceUrl + "user/" +kinveyAppID + "/",
-        headers : {"Authorization" : "Basic " + authBase64},
-        data:
-        {
             username: $("#registerUser").val(),
             password: $("#registerPass").val()
         },
@@ -203,4 +119,8 @@ $(document).ajaxStart(function(){
 }).ajaxStop(function(){
     $("#loadingBox").hide()
 });
+function gottaChangeSmth()
+{
+    let five =5;
+};
 
